5880737748c757fb5eeebd511a2da75f186b1e17
buraktitiz <titizburak10@gmail.com>
2022-11-29 21:33:08 +0300

    Publish İşlemleri

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'megalodonx'
+import { ExampleComponent, Button } from 'megalodonx'
 import 'megalodonx/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+    <ExampleComponent text="Create React Library Example 😄" />
+    <Button text="Click" onClick={() =>alert("Hatalı giriş")}/>
+    </>
+  )
 }
 
 export default App
